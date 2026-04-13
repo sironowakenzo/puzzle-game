4e193d825c25b7b6e8f415df56486761604df159
sironowakenzo <sironowakenzo9@gmail.com>
2026-04-14 06:01:09 +0900

    Change cache name and implement cache cleanup
    Updated cache name and added activation event to manage old caches.

--- sw.js
+++ sw.js
@@ -1,17 +1,27 @@
-const CACHE_NAME = "text-dungeon-v1";
+const CACHE_NAME = "tile-grove-v2";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./game.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./manifest.webmanifest"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
   );
 });
 
